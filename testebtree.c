2a0faf77063757e7a62c29c096408eee8bd9bec5
Willy Tarreau <w@1wt.eu>
2007-07-08 09:52:25 +0200

    [TESTS] update test programs.
    Provided testtree.c and testtree64.c too.

--- testebtree.c
+++ testebtree.c
@@ -1,19 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/time.h>
 
-#include "../../../public/haproxy-1.3/include/common/time.h"
-
 #include "ebtree.h"
-
-//#include "../../../public/haproxy-1.3/include/common/defaults.h"
-//#include "../../../public/haproxy-1.3/include/common/config.h"
-//#include "../../../public/haproxy-1.3/include/common/standard.h"
-//#include "../../../public/haproxy-1.3/include/common/time.h"
 
 #define rdtscll(val) \
      __asm__ __volatile__("rdtsc" : "=A" (val))
-static unsigned long long start, end;
+
+static inline struct timeval *tv_now(struct timeval *tv) {
+	gettimeofday(tv, NULL);
+	return tv;
+}
+
+static inline unsigned long tv_ms_elapsed(const struct timeval *tv1, const struct timeval *tv2) {
+	unsigned long ret;
+
+	ret  = ((signed long)(tv2->tv_sec  - tv1->tv_sec))  * 1000;
+	ret += ((signed long)(tv2->tv_usec - tv1->tv_usec)) / 1000;
+	return ret;
+}
+
+
+static unsigned long long start, calibrate, end, cycles;
 
 
 EB32_TREE_HEAD(root);
@@ -91,8 +100,16 @@ int main(int argc, char **argv) {
 	    //unsigned long long x = ((unsigned long long)h << 16) + l;
 	    unsigned long x = random();     // triggers worst case cache patterns
 
+	    //x = i & 16383;// ^ (1 << (i&31));//(i < 32) ? (1 << i) : 1/*(i & 1023)*/;
+	    //x = 1UL << (i&31);
+	    //x = (i >> 10) << 20 | (i & 1023);
+	    x = ((i >> 10) << 20) + (i & 1023) * 3;
+	    //x = rev32(i);
+	    //x = (x >> 10) << 20 | (x & 1023);
+	    //x = (x >> 16) ^ (x << 16);
+	    //x = i;
 	    node = (struct eb32_node *)calloc(1,sizeof(*node));
-	    node->val = /*x;//total-i-1;//*/(x>>10)&65535;//i&65535;//(x>>8)&65535;//rev32(i);//i&32767;//x;//i ^ (long)lastnode;
+	    node->val = x;//*x;//total-i-1;//*/(x>>10)&65535;//i&65535;//(x>>8)&65535;//rev32(i);//i&32767;//x;//i ^ (long)lastnode;
 	    node->node.leaf_p = (void *)lastnode;
 	    lastnode = node;
 	}
@@ -101,48 +118,52 @@ int main(int argc, char **argv) {
 	printf("%llu cycles/ent\n", (end - start)/total);
 
 	printf("Timing %d insert... ", total);
-	rdtscll(start);
+	cycles = 0;
 	for (i = 0; i < total; i++) {
 	    node = lastnode;
 	    lastnode = (void *)node->node.leaf_p;
+	    rdtscll(start); rdtscll(calibrate); // account for the time spent calling rdtsc too !
 	    eb32_insert(&root, node);
+	    rdtscll(end); cycles += (end - calibrate) - (calibrate - start);
 	    if (!node->node.leaf_p)
-		neighbours++;
+	       neighbours++;
 	    else if (node->node.bit)
-		links_used++;
+	       links_used++;
 	}
-	rdtscll(end);
 	tv_now(&t_insert);
-	printf("%llu cycles/ent\n", (end - start)/total);
+	printf("%llu cycles/ent\n", cycles/total);
 	printf("%lu jumps during insertion = %llu jumps/1000 ins\n", total_jumps, (1000ULL*total_jumps)/total);
     }
 
     printf("Looking up %d entries... ", total);
-    rdtscll(start);
+    cycles = 0;
     for (i = 0; i < total; i++) {
 	unsigned long long x = i;//random();//(random()>>10)&65535;//(i << 16) + ((random() & 63ULL) << 32) + (random() % 1000);
+	rdtscll(start); rdtscll(calibrate); // account for the time spent calling rdtsc too !
 	node = eb32_lookup(&root, x);
+	rdtscll(end); cycles += (end - calibrate) - (calibrate - start);
 	if (node && (node->val != (int)x)) {
 	    printf("node = %p, wanted = %d, returned = %d\n", node, (int)x, node->val);
 	}
 	//if (!node)
 	//    printf("wanted = %d\n", (int)x);
     }
-    rdtscll(end);
     tv_now(&t_lookup);
-    printf("%llu cycles/ent\n", (end - start)/total);
+    printf("%llu cycles/ent\n", cycles/total);
 
     printf("Walking forwards %d entries... ", total);
-    rdtscll(start);
+
+    cycles = 0;
     node = eb_first(&root);
     while (node) {
 	//printf("node = %p, node->val = 0x%08x, link_p=%p, leaf_p=%p, bit=%d, leaf_p->bit=%d\n",
 	//       node, node->val, node->node.link_p, node->node.leaf_p, node->node.bit,
 	//       node->node.leaf_p ? node->node.leaf_p->bit : -1);
+	rdtscll(start); rdtscll(calibrate); // account for the time spent calling rdtsc too !
 	node = eb_next(node);
+	rdtscll(end); cycles += (end - calibrate) - (calibrate - start);
     }
-    rdtscll(end);
-    printf("%llu cycles/ent\n", (end - start)/total);
+    printf("%llu cycles/ent\n", cycles/total);
 
     printf("Walking backwards %d entries... ", total);
     rdtscll(start);
@@ -209,47 +230,12 @@ int main(int argc, char **argv) {
     printf("eb_first now returns %p\n", node);
 
     printf("total=%u, links=%lu, neighbours=%lu entries, total_jumps=%lu\n", total, links_used, neighbours, total_jumps);
-    printf("random+malloc =%lu ms\n", __tv_ms_elapsed(&t_start, &t_random));
-    printf("insert        =%lu ms\n", __tv_ms_elapsed(&t_random, &t_insert));
-    printf("lookup        =%lu ms\n", __tv_ms_elapsed(&t_insert, &t_lookup));
-    printf("walk          =%lu ms\n", __tv_ms_elapsed(&t_lookup, &t_walk));
-    printf("move          =%lu ms\n", __tv_ms_elapsed(&t_walk, &t_move));
-    printf("delete        =%lu ms\n", __tv_ms_elapsed(&t_move, &t_delete));
+    printf("random+malloc =%lu ms\n", tv_ms_elapsed(&t_start, &t_random));
+    printf("insert        =%lu ms\n", tv_ms_elapsed(&t_random, &t_insert));
+    printf("lookup        =%lu ms\n", tv_ms_elapsed(&t_insert, &t_lookup));
+    printf("walk          =%lu ms\n", tv_ms_elapsed(&t_lookup, &t_walk));
+    printf("move          =%lu ms\n", tv_ms_elapsed(&t_walk, &t_move));
+    printf("delete        =%lu ms\n", tv_ms_elapsed(&t_move, &t_delete));
 
-#if 0
-    tv_now(&t_walk_start);
-#define MAXLEN 64
-    {
-	void *stack[MAXLEN];
-	int slen;
-	struct leaf *leaf;
-	void *list;
-	tree64_foreach/*_destructive*/(&root, list, stack, slen) {
-	    node_count++;
-	    foreach_dlist_item_cst(leaf, list, struct leaf*, list) {
-		  val_count++;
-		  //printf("%016llx\n", leaf->val);
-		  //printf("%08x\n", (unsigned)leaf->val);
-		  //pool_free(leaf, leaf);
-	    }
-	    //((struct ultree*)stack[slen])->data = NULL;
-	}
-    }
-    
-    tv_now(&t_walk_stop);
-
-    printf("time: alloc:%ld ms, random:%ld ms, insert:%ld-%ld=%ld ms (%ld k.insert/s), lookup: %ld-%ld=%ld ms, walk=%ld ms\n",
-	   tv_delta_ms(&t_start, &t_alloc),
-	   tv_delta_ms(&t_alloc, &t_random),
-	   tv_delta_ms(&t_random, &t_insert), tv_delta_ms(&t_alloc, &t_random), 
-	   tv_delta_ms(&t_random, &t_insert) - tv_delta_ms(&t_alloc, &t_random),
-	   total / (tv_delta_ms(&t_random, &t_insert) - tv_delta_ms(&t_alloc, &t_random)),
-	   tv_delta_ms(&t_insert, &t_lookup), tv_delta_ms(&t_alloc, &t_random),
-	   tv_delta_ms(&t_insert, &t_lookup) - tv_delta_ms(&t_alloc, &t_random),
-	   tv_delta_ms(&t_walk_start, &t_walk_stop));
-    
-    printf("total=%d, node_count=%d, val_count=%d, node_lookup=%d, node_right_lookup=%d (left=%d)\n", total, node_count, val_count,
-	   node_lookup, node_right_lookup, node_lookup - node_right_lookup);
-#endif
     return 0;
 }
