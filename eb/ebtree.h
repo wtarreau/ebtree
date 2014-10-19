3065c7b8bc88e75778c67a94423a11cbbf1f61aa
Willy Tarreau <w@1wt.eu>
2014-10-20 00:49:36 +0200

    CLEANUP: remove undefs from ebtree.h
    Since ebtree.h is protected against multiple inclusions, we don't need
    to #undef the ebx* macros, so better not do it to more easily detect
    double definitions.

--- eb/ebtree.h
+++ eb/ebtree.h
@@ -8,60 +8,37 @@
 
 #include "ebatree.h"
 
-#undef eb_root
 #define eb_root eba_root
-#undef eb_troot_t
 #define eb_troot_t eba_troot_t
-#undef eb_node
 #define eb_node eba_node
-#undef eb_link_t
 #define eb_link_t eba_link_t
 
 #define EB_ROOT            EBA_ROOT
 #define EB_ROOT_UNIQUE     EBA_ROOT_UNIQUE
 #define EB_TREE_HEAD(name) EBA_TREE_HEAD(name)
 
-#undef eb_is_empty
 #define eb_is_empty eba_is_empty
-#undef eb_is_dup
 #define eb_is_dup eba_is_dup
 
-#undef eb_untag
 #define eb_untag eba_untag
-#undef eb_dotag
 #define eb_dotag eba_dotag
-#undef eb_clrtag
 #define eb_clrtag eba_clrtag
-#undef eb_gettag
 #define eb_gettag eba_gettag
 
-#undef eb_first
 #define eb_first eba_first
-#undef eb_last
 #define eb_last eba_last
-#undef eb_next
 #define eb_next eba_next
-#undef eb_next_dup
 #define eb_next_dup eba_next_dup
-#undef eb_next_unique
 #define eb_next_unique eba_next_unique
-#undef eb_prev
 #define eb_prev eba_prev
-#undef eb_prev_dup
 #define eb_prev_dup eba_prev_dup
-#undef eb_prev_unique
 #define eb_prev_unique eba_prev_unique
-#undef eb_walk_down
 #define eb_walk_down eba_walk_down
 
-#undef __eb_delete
 #define __eb_delete __eba_delete
-#undef __eb_insert_dup
 #define __eb_insert_dup __eba_insert_dup
 
-#undef eb_delete
 #define eb_delete eba_delete
-#undef eb_insert_dup
 #define eb_insert_dup eba_insert_dup
 
 #define EB32_ROOT       EB_ROOT
